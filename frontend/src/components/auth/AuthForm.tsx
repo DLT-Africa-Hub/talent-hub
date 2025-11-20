3fe19e5c711d5e4f18f7de5c2f50b12f55c8334c
anaboy01 <aliyuanate016@gmail.com>
2025-11-20 22:21:08 +0100

    refactor: auth form role tab

--- frontend/src/components/auth/AuthForm.tsx
+++ frontend/src/components/auth/AuthForm.tsx
@@ -61,8 +61,16 @@ const AuthForm: React.FC<AuthFormProps> = ({
       <div className="absolute inset-0 bg-white/50"></div>
 
       <div className="flex flex-col items-center justify-between  lg:justify-center gap-6 z-10 py-12 px-5 w-full max-w-[542px] mx-auto">
-        {/* ---------- ROLE TABS (ONLY ON REGISTER) ---------- */}
-        {mode === 'register' && (
+     
+
+        {/* ---------- HEADER ---------- */}
+        <div className="flex flex-col w-full text-center">
+          <h2 className="font-semibold text-[32px] text-[#1C1C1C]">{title}</h2>
+          <p className="font-normal text-[18px] text-[#1C1C1CBF]">{subtitle}</p>
+        </div>
+
+           {/* ---------- ROLE TABS (ONLY ON REGISTER) ---------- */}
+           {mode === 'register' && (
           <div className="w-full mb-2">
             <div className="flex border-b border-gray-200  rounded-t-xl">
               {/* Graduate */}
@@ -93,12 +101,6 @@ const AuthForm: React.FC<AuthFormProps> = ({
             </div>
           </div>
         )}
-
-        {/* ---------- HEADER ---------- */}
-        <div className="flex flex-col w-full text-center">
-          <h2 className="font-semibold text-[32px] text-[#1C1C1C]">{title}</h2>
-          <p className="font-normal text-[18px] text-[#1C1C1CBF]">{subtitle}</p>
-        </div>
 
         <form onSubmit={onSubmit} className="flex flex-col gap-4 w-full">
           <div className="flex flex-col gap-4">
